fdf40360355d01555e98a09b0b8df44ef833ad41
Estela Alves de Moraes <estela.moraes2@portalsesisp.org.br>
2024-05-28 20:10:22 +0000

    Added 2 files to /model/DAO

---
+++ model/DAO/controller_lance.js
@@ -0,0 +1,353 @@
+/***********************************************************************
+ * Objetivo: Arquivo responsável pelas validações e consistências de dados de 'lances'.
+ * Data: 21/05/2023
+ * Autor: Estela Alves de Moraes
+ * Versão: 1.0
+***********************************************************************/
+
+const lancesDAO = require('../model/DAO/lance.js')
+const controllerLote = require('../controller/controller_lote.js')
+const usuarioDAO = require('../model/DAO/usuario.js')
+const message = require('../module/config.js')
+
+const getListarLances = async function () {
+
+    try {
+
+        let lancesJSON = {}
+
+        let dadosLances = await lancesDAO.selectAllLances()
+
+        await Promise.all(dadosLances.map(async lance =>{
+            let lanceLote = await controllerLote.getBuscarLote(lance.lote)
+            lance.lote = lanceLote.lote
+        }))
+        await Promise.all(dadosLances.map(async lance =>{
+            let usuarioLance = await usuarioDAO.selectByIdUser(lance.usuario)
+            lance.usuario = usuarioLance
+        }))
+
+        if (dadosLances) {
+            if (dadosLances.length > 0) {
+                lancesJSON.lances = dadosLances
+                lancesJSON.quantidade = dadosLances.length
+                lancesJSON.status_code = 200
+
+                return lancesJSON
+            } else {
+                return message.ERROR_NOT_FOUND
+            }
+        } else {
+            return message.ERROR_INTERNAL_SERVER_DB
+        }
+
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+const getBuscarLance = async function (id) {
+
+    let idLance = id
+
+    try {
+
+        if (idLance == undefined || isNaN(idLance) || idLance == "") {
+            return message.ERROR_INVALID_ID
+        } else {
+            let lanceJSON = {}
+
+            let dadosLance = await lancesDAO.selectLanceById(idLance)
+
+            await Promise.all(dadosLance.map(async lance =>{
+                let lanceLote = await controllerLote.getBuscarLote(lance.lote)
+                lance.lote = lanceLote.lote
+            }))
+            await Promise.all(dadosLance.map(async lance =>{
+                let usuarioLance = await usuarioDAO.selectByIdUser(lance.usuario)
+                lance.usuario = usuarioLance
+            }))
+
+            if (dadosLance) {
+                if (dadosLance.length > 0) {
+                    lanceJSON.lance = dadosLance
+                    lanceJSON.status_code = 200
+
+                    return lanceJSON
+                } else {
+                    return message.ERROR_NOT_FOUND
+                }
+            } else {
+                return message.ERROR_INTERNAL_SERVER_DB
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+const getArrematesUsuario = async function (id){
+    try {
+
+        if (id == undefined || isNaN(id) || id == "") {
+            return message.ERROR_INVALID_ID
+        } else {
+            let lanceJSON = {}
+
+            let dadosLotesUsuario = await lancesDAO.selectLancesUsuario(id)
+
+            let lancesArray = []
+            await Promise.all(dadosLotesUsuario.map(async lote =>{
+                let dadosLance = await lancesDAO.selectArrematante(lote.id)
+                lancesArray.push(dadosLance)
+            }))
+            
+            await Promise.all(lancesArray.map(async lance =>{
+                let lanceLote = await controllerLote.getBuscarLote(lance.lote)
+                lance.lote = lanceLote.lote
+            }))
+            await Promise.all(lancesArray.map(async lance =>{
+                let usuarioLance = await usuarioDAO.selectByIdUser(lance.usuario)
+                lance.usuario = usuarioLance
+            }))
+
+            if (lancesArray) {
+                if (lancesArray.length > 0) {
+                    lanceJSON.lance = lancesArray
+                    lanceJSON.status_code = 200
+
+                    return lanceJSON
+                } else {
+                    return message.ERROR_NOT_FOUND
+                }
+            } else {
+                return message.ERROR_INTERNAL_SERVER_DB
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+const getBuscarArrematante = async function (id) {
+
+    let idLote = id
+
+    try {
+
+        if (idLote == undefined || isNaN(idLote) || idLote == "") {
+            return message.ERROR_INVALID_ID
+        } else {
+            let lanceJSON = {}
+
+            let dadosLance = await lancesDAO.selectArrematante(idLote)
+
+            await Promise.all(dadosLance.map(async lance =>{
+                let lanceLote = await controllerLote.getBuscarLote(lance.lote)
+                lance.lote = lanceLote.lote
+            }))
+            await Promise.all(dadosLance.map(async lance =>{
+                let usuarioLance = await usuarioDAO.selectByIdUser(lance.usuario)
+                lance.usuario = usuarioLance
+            }))
+
+            if (dadosLance) {
+                if (dadosLance.length > 0) {
+                    lanceJSON.lance = dadosLance
+                    lanceJSON.status_code = 200
+
+                    return lanceJSON
+                } else {
+                    return message.ERROR_NOT_FOUND
+                }
+            } else {
+                return message.ERROR_INTERNAL_SERVER_DB
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+const getFiltrarLance = async function (parametros) {
+
+    try {
+            let lanceJSON = {}
+
+            let dadosLance = await lancesDAO.selectLanceByFiltro(parametros)
+            console.log(dadosLance)
+
+            await Promise.all(dadosLance.map(async lance =>{
+                let lanceLote = await controllerLote.getBuscarLote(lance.lote)
+                lance.lote = lanceLote.lote
+            }))
+            await Promise.all(dadosLance.map(async lance =>{
+                let usuarioLance = await usuarioDAO.selectByIdUser(lance.usuario)
+                lance.usuario = usuarioLance
+            }))
+
+            if (dadosLance) {
+                if (dadosLance.length > 0) {
+                    lanceJSON.lance = dadosLance
+                    lanceJSON.status_code = 200
+
+                    return lanceJSON
+                } else {
+                    return message.ERROR_NOT_FOUND
+                }
+            } else {
+                return message.ERROR_INTERNAL_SERVER_DB
+            }
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+const setInserirLance = async function (dadosBody, contentType) {
+
+    let dadosLance = dadosBody
+
+    try {
+
+        if (String(contentType).toLowerCase() == 'application/json') {
+
+            let novoLanceJSON = {}
+
+            if (
+                dadosLance.data == "" || dadosLance.data == undefined || dadosLance.data == null || dadosLance.data.length > 10 ||
+                dadosLance.valor == "" || dadosLance.valor == undefined || dadosLance.valor == null || isNaN(dadosLance.valor) ||
+                dadosLance.lote == "" || dadosLance.lote == undefined || dadosLance.lote == null || isNaN(dadosLance.lote) ||
+                dadosLance.usuario == "" || dadosLance.usuario == undefined || dadosLance.usuario == null || isNaN(dadosLance.usuario) 
+            ) {
+                return message.ERROR_REQUIRED_FIELDS
+            } else {
+
+                let novoLance = await lancesDAO.insertLance(dadosLance)
+                let novoLanceId = await lancesDAO.selectLastInsertId()
+
+                let dadosNovoLance = await getBuscarLance(novoLanceId)
+
+                if (novoLance) {
+                    novoLanceJSON.lance = dadosNovoLance.lance
+                    novoLanceJSON.status_code = message.SUCCESS_CREATED_ITEM.status_code
+                    novoLanceJSON.status = message.SUCCESS_CREATED_ITEM.status
+                    novoLanceJSON.message = message.SUCCESS_CREATED_ITEM.message
+
+                    return novoLanceJSON
+
+                } else {
+                    return message.ERROR_INTERNAL_SERVER_DB
+                }
+            }
+
+        } else {
+            return message.ERROR_CONTENT_TYPE
+        }
+
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+
+}
+
+const setAtualizarLance = async function (id, dadosBody, contentType) {
+
+    let dadosLance = dadosBody
+    let idLance = id
+
+    
+    try {
+
+        if (String(contentType).toLowerCase() == 'application/json') {
+
+            if (idLance == undefined || isNaN(idLance) || idLance == "") {
+                return message.ERROR_INVALID_ID
+            } else {
+
+                let lanceAtualizadaJSON = {}
+
+                if (dadosLance.nome == "" || dadosLance.nome == undefined || dadosLance.nome == null || dadosLance.nome.length > 45 ||
+                dadosLance.data_inicio == "" || dadosLance.data_inicio == undefined || dadosLance.data_inicio == null || dadosLance.data_inicio.length > 10 ||
+                dadosLance.descricao == "" || dadosLance.descricao == undefined || dadosLance.descricao == null || 
+                dadosLance.reserva == "" || dadosLance.reserva == undefined || dadosLance.reserva == null || isNaN(dadosLance.reserva) ||
+                dadosLance.status == "" || dadosLance.status == undefined || dadosLance.status == null || isNaN(dadosLance.status) ||
+                dadosLance.leilao == "" || dadosLance.leilao == undefined || dadosLance.leilao == null || isNaN(dadosLance.leilao)  ) {
+
+                    return message.ERROR_REQUIRED_FIELDS
+                } else {
+
+
+                    let lanceAtualizada = await lancesDAO.updateLance(dadosBody, idLance)
+
+
+                    // let dadosLanceAtualizado = await getBuscarLance(idLance)
+
+                    if (lanceAtualizada) {
+                        // lanceAtualizadaJSON.lance = dadosLanceAtualizado.lance
+                        lanceAtualizadaJSON.status_code = message.SUCCESS_UPDATED_ITEM.status_code
+                        lanceAtualizadaJSON.status = message.SUCCESS_UPDATED_ITEM.status
+                        lanceAtualizadaJSON.message = message.SUCCESS_UPDATED_ITEM.message
+
+                        return lanceAtualizadaJSON
+
+                    } else {
+                        return message.ERROR_INTERNAL_SERVER_DB
+                    }
+                }
+            }
+        } else {
+            return message.ERROR_CONTENT_TYPE
+        }
+
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+
+}
+
+const setExcluirLance = async function (id) {
+
+    let idLance = id
+
+    try {
+
+        if (idLance == undefined || isNaN(idLance) || idLance == "") {
+            return message.ERROR_INVALID_ID
+        } else {
+            let lanceExcluidaJSON = {}
+
+            let dadosLanceExcluida = await lancesDAO.deleteLance(idLance)
+
+            if (dadosLanceExcluida) {
+                lanceExcluidaJSON.status_code = message.SUCCESS_DELETED_ITEM.status_code
+                lanceExcluidaJSON.status = message.SUCCESS_DELETED_ITEM.status
+                lanceExcluidaJSON.message = message.SUCCESS_DELETED_ITEM.message
+
+                    return lanceExcluidaJSON
+            } else {
+                return message.ERROR_INTERNAL_SERVER_DB
+            }
+        }
+    } catch (error) {
+
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+module.exports = {
+    getListarLances,
+    getBuscarLance,
+    getBuscarArrematante,
+    getArrematesUsuario,
+    getFiltrarLance,
+    setInserirLance,
+    setAtualizarLance,
+    setExcluirLance
+}
---
+++ model/DAO/controller_lote.js
@@ -0,0 +1,247 @@
+/***********************************************************************
+ * Objetivo: Arquivo responsável pelas validações e consistências de dados de 'lotes'.
+ * Data: 21/05/2023
+ * Autor: Estela Alves de Moraes
+ * Versão: 1.0
+***********************************************************************/
+
+const lotesDAO = require('../model/DAO/lote.js')
+const controllerSubcategorias= require('../controller/controller_subcategoria.js')
+const controllerLeilao = require('../controller/controller_leilao.js')
+const controllerStatus = require('../controller/controller_status.js')
+const message = require('../module/config.js')
+
+const getListarLotes = async function () {
+
+    try {
+
+        let lotesJSON = {}
+
+        let dadosLotes = await lotesDAO.selectAllLotes()
+
+        await Promise.all(dadosLotes.map(async lote =>{
+            let statusLote = await controllerStatus.getListarStatusById(lote.status)
+            lote.status = statusLote.status
+        }))
+        await Promise.all(dadosLotes.map(async lote =>{
+            let subcategoriaLote = await controllerSubcategorias.getBuscarSubcategoriasByLote(lote.id)
+            lote.subcategoria = subcategoriaLote.subcategorias
+        }))
+
+        if (dadosLotes) {
+            if (dadosLotes.length > 0) {
+                lotesJSON.lotes = dadosLotes
+                lotesJSON.quantidade = dadosLotes.length
+                lotesJSON.status_code = 200
+
+                return lotesJSON
+            } else {
+                return message.ERROR_NOT_FOUND
+            }
+        } else {
+            return message.ERROR_INTERNAL_SERVER_DB
+        }
+
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+const getBuscarLote = async function (id) {
+
+    let idLote = id
+
+    try {
+
+        if (idLote == undefined || isNaN(idLote) || idLote == "") {
+            return message.ERROR_INVALID_ID
+        } else {
+            let loteJSON = {}
+
+            let dadosLote = await lotesDAO.selectLoteById(idLote)
+
+            
+            await Promise.all(dadosLote.map(async lote =>{
+                let statusLote = await controllerStatus.getListarStatusById(lote.status)
+                lote.status = statusLote.status
+            }))
+            await Promise.all(dadosLote.map(async lote =>{
+                let subcategoriaLote = await controllerSubcategorias.getBuscarSubcategoriasByLote(lote.id)
+                lote.subcategoria = subcategoriaLote.subcategorias
+            }))
+
+            if (dadosLote) {
+                if (dadosLote.length > 0) {
+                    loteJSON.lote = dadosLote
+                    loteJSON.status_code = 200
+
+                    return loteJSON
+                } else {
+                    return message.ERROR_NOT_FOUND
+                }
+            } else {
+                return message.ERROR_INTERNAL_SERVER_DB
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+
+const setInserirLote = async function (dadosBody, contentType) {
+
+    let dadosLote = dadosBody
+
+    try {
+
+        if (String(contentType).toLowerCase() == 'application/json') {
+
+            let novoLoteJSON = {}
+
+            if (dadosLote.nome == "" || dadosLote.nome == undefined || dadosLote.nome == null || dadosLote.nome.length > 45 ||
+                dadosLote.data_inicio == "" || dadosLote.data_inicio == undefined || dadosLote.data_inicio == null || dadosLote.data_inicio.length > 10 ||
+                dadosLote.descricao == "" || dadosLote.descricao == undefined || dadosLote.descricao == null || 
+                dadosLote.reserva == "" || dadosLote.reserva == undefined || dadosLote.reserva == null || isNaN(dadosLote.reserva) ||
+                dadosLote.status == "" || dadosLote.status == undefined || dadosLote.status == null || isNaN(dadosLote.status) ||
+                dadosLote.leilao == "" || dadosLote.leilao == undefined || dadosLote.leilao == null || isNaN(dadosLote.leilao) 
+            ) {
+                return message.ERROR_REQUIRED_FIELDS
+            } else {
+
+                let novoLote = await lotesDAO.insertLote(dadosLote)
+                let novoLoteId = await lotesDAO.selectLastInsertId()
+
+                let novaCategoriaLote
+                dadosLote.categoria.forEach(async categoria =>{
+                    novaCategoriaLote = await lotesDAO.insertCategoriaLote(categoria, novoLoteId)
+                })
+                let dadosNovoLote = await getBuscarLote(novoLoteId)
+
+                if (novoLote) {
+                    novoLoteJSON.lote = dadosNovoLote.lote
+                    novoLoteJSON.status_code = message.SUCCESS_CREATED_ITEM.status_code
+                    novoLoteJSON.status = message.SUCCESS_CREATED_ITEM.status
+                    novoLoteJSON.message = message.SUCCESS_CREATED_ITEM.message
+
+                    return novoLoteJSON
+
+                } else {
+                    return message.ERROR_INTERNAL_SERVER_DB
+                }
+            }
+
+        } else {
+            return message.ERROR_CONTENT_TYPE
+        }
+
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+
+}
+
+const setAtualizarLote = async function (id, dadosBody, contentType) {
+
+    let dadosLote = dadosBody
+    let idLote = id
+
+    
+    try {
+
+        if (String(contentType).toLowerCase() == 'application/json') {
+
+            if (idLote == undefined || isNaN(idLote) || idLote == "") {
+                return message.ERROR_INVALID_ID
+            } else {
+
+                let loteAtualizadaJSON = {}
+
+                if (dadosLote.nome == "" || dadosLote.nome == undefined || dadosLote.nome == null || dadosLote.nome.length > 45 ||
+                dadosLote.data_inicio == "" || dadosLote.data_inicio == undefined || dadosLote.data_inicio == null || dadosLote.data_inicio.length > 10 ||
+                dadosLote.descricao == "" || dadosLote.descricao == undefined || dadosLote.descricao == null || 
+                dadosLote.reserva == "" || dadosLote.reserva == undefined || dadosLote.reserva == null || isNaN(dadosLote.reserva) ||
+                dadosLote.status == "" || dadosLote.status == undefined || dadosLote.status == null || isNaN(dadosLote.status) ||
+                dadosLote.leilao == "" || dadosLote.leilao == undefined || dadosLote.leilao == null || isNaN(dadosLote.leilao)  ) {
+
+                    return message.ERROR_REQUIRED_FIELDS
+                } else {
+
+                    //consertar caso não exista nenhuma subcategoria ou esteja adicionando mais do que já tem
+                    let categoriasAntigas = await controllerSubcategorias.getBuscarSubcategoriasByLote(idLote)
+                    let count = 0
+                    let subcategoriaAtualizada
+                    categoriasAntigas.subcategorias.forEach(async categoriaAntiga =>{
+                        subcategoriaAtualizada = await lotesDAO.updateCategoriaLote(dadosLote.categoria[count], categoriaAntiga.id, idLote)
+                        count ++
+                    })
+
+                    let loteAtualizada = await lotesDAO.updateLote(dadosBody, idLote)
+
+
+                    // let dadosLoteAtualizado = await getBuscarLote(idLote)
+
+                    if (loteAtualizada) {
+                        // loteAtualizadaJSON.lote = dadosLoteAtualizado.lote
+                        loteAtualizadaJSON.status_code = message.SUCCESS_UPDATED_ITEM.status_code
+                        loteAtualizadaJSON.status = message.SUCCESS_UPDATED_ITEM.status
+                        loteAtualizadaJSON.message = message.SUCCESS_UPDATED_ITEM.message
+
+                        return loteAtualizadaJSON
+
+                    } else {
+                        return message.ERROR_INTERNAL_SERVER_DB
+                    }
+                }
+            }
+        } else {
+            return message.ERROR_CONTENT_TYPE
+        }
+
+    } catch (error) {
+        console.log(error)
+        return message.ERROR_INTERNAL_SERVER
+    }
+
+}
+
+const setExcluirLote = async function (id) {
+
+    let idLote = id
+
+    try {
+
+        if (idLote == undefined || isNaN(idLote) || idLote == "") {
+            return message.ERROR_INVALID_ID
+        } else {
+            let loteExcluidaJSON = {}
+
+            let dadosCategoriaExcluida = await lotesDAO.deleteCategoriaLote(idLote)
+            let dadosLoteExcluida = await lotesDAO.deleteLote(idLote)
+
+            if (dadosCategoriaExcluida && dadosLoteExcluida) {
+                loteExcluidaJSON.status_code = message.SUCCESS_DELETED_ITEM.status_code
+                loteExcluidaJSON.status = message.SUCCESS_DELETED_ITEM.status
+                loteExcluidaJSON.message = message.SUCCESS_DELETED_ITEM.message
+
+                    return loteExcluidaJSON
+            } else {
+                return message.ERROR_INTERNAL_SERVER_DB
+            }
+        }
+    } catch (error) {
+
+        return message.ERROR_INTERNAL_SERVER
+    }
+}
+
+module.exports = {
+    getListarLotes,
+    getBuscarLote,
+    setInserirLote,
+    setAtualizarLote,
+    setExcluirLote
+}
