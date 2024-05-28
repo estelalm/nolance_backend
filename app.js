b59106ced89b1a9586756a849718f8c6d6a217f8
MARIA ALMEIDA <maria.almeida39@portalsesisp.org.br>
2024-05-28 20:25:56 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ const controllerComitentes = require('./controller/controller_comitente.js')
 const controllerLeiloes = require('./controller/controller_leilao.js')
 const controllerLotes = require('./controller/controller_lote')
 const controllerLances = require('./controller/controller_lance.js')
+const controllerLeiloes = require('./controller/controller_leilao.js')
+const controllerPagamentos = require('./controller/controller_pagamento.js')
 const { log } = require('console')
 /******************************************************************************************/
 
