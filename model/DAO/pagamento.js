4f163c4a3c5c8aa974e8c84fdc310f01446ddf64
MARIA ALMEIDA <maria.almeida39@portalsesisp.org.br>
2024-06-04 01:03:05 +0000

    Updated pagamento.js

--- model/DAO/pagamento.js
+++ model/DAO/pagamento.js
@@ -1,34 +1,71 @@
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const selectAllPayments = async () => {
-    try {
-        let sql = `select * from tbl_pagamento`
+  try {
+    let sql = `select * from tbl_pagamento`;
 
-        let rsPayments = await prisma.$queryRawUnsafe(sql)
+    let rsPayments = await prisma.$queryRawUnsafe(sql);
 
-        return rsPayments
-    } catch (error) {
-        return false
-    }
-}
+    return rsPayments;
+  } catch (error) {
+    return false;
+  }
+};
 
-const insertPayment = async (dados) => {
-    try {
-        let sql = `insert into tbl_pagamento (lance_id, tbl_forma_pagamento_id, data_pagamento) values
-                (${dados.lance_id}, ${dados.tbl_forma_pagamento_id}, ${dados.data_pagamento})`
+const createPayment = async (dados, intent_payment_id) => {
+  try {
+    let sql = `insert into tbl_pagamento (lance_id, intent_payment_id ,tbl_forma_pagamento_id, completado, data_pagamento ) values
+                    (   ${dados.lance_id} ,
+                         ${intent_payment_id}, 
+                         ${dados.forma_pagamento_id}, 
+                         false, 
+                         '${dados.data_pagamento}')`;
 
-        let result = await prisma.$executeRawUnsafe(sql)
+    let result = await prisma.$executeRawUnsafe(sql);
 
-        if (result) return true
-        else return false
+    if (result) return true;
+    else return false;
+  } catch (error) {
+    return false;
+  }
+};
 
-    } catch (error) {
-        return false
-    }
-}
+const findByIntentPayment = async (intent_payment_id) => {
+  try {
+    let sql = `select * from tbl_pagamento where intent_payment_id = ${intent_payment_id}`;
+
+    let result = await prisma.$queryRawUnsafe(sql);
+
+    if (result) return true;
+    else return false;
+  } catch (error) {
+    return false;
+  }
+};
+
+const completePayment = async (dados, intent_payment_id) => {
+  try {
+    let sql = `UPDATE tbl_pagamento SET 
+                    lance_id = ${dados.lance_id},
+                    intent_payment_id = ${intent_payment_id},
+                    tbl_forma_pagamento_id = ${dados.forma_pagamento_id},
+                    completado = true,
+                    data_pagamento = '${dados.data_pagamento}'
+                WHERE intent_payment_id = ${intent_payment_id}  `;
+
+    let result = await prisma.$executeRawUnsafe(sql);
+
+    if (result) return true;
+    else return false;
+  } catch (error) {
+    return false;
+  }
+};
 
 module.exports = {
-    selectAllPayments,
-    insertPayment
-}
+  selectAllPayments,
+  completePayment,
+  findByIntentPayment,
+  createPayment,
+};
