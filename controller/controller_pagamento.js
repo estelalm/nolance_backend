20908f34697412f237fb71a3127941a432672b53
MARIA ALMEIDA <maria.almeida39@portalsesisp.org.br>
2024-06-04 20:30:56 +0000

    arrumar confirmação :(

--- controller/controller_pagamento.js
+++ controller/controller_pagamento.js
@@ -56,28 +56,32 @@ const createPaymentIntent = async (arrematante, idLote) => {
         const dadosLote = await loteDAO.selectLoteById(idLote)
         const imagensLote = await loteDAO.selectImagemLote(idLote)
         dadosLote.imagens = imagensLote
-
-        const result = await stripe.makePayment(arrematante.lance, dadosLote);
-  
+     
+        const result = await stripe.makePayment(arrematante.lance[0], dadosLote);
+        
         return result;
 
     } catch (error) {
+        console.log(error);
         return false
     }
 };
   
 const confirmPayment = async (order, sig) => {
+    const pedido = JSON.parse(order.toString())
+
     try {
+        const event = await stripe.handlePayment(order, sig);
+         
+        if (!event) return 
+
         if(order.lance_id == "" || order.lance_id == undefined|| order.lance_id == null || isNaN(order.lance_id) ||
         order.intent_payment_id == ""|| order.intent_payment_id == undefined || order.intent_payment_id == null ||
-        order.tbl_forma_pagamento_id== "" || order.tbl_forma_pagamento_id == undefined|| order.tbl_forma_pagamento_id == null || isNaN(order.tbl_forma_pagamento_id) ||
+        order.tbl_forma_pagamento_id == "" || order.tbl_forma_pagamento_id == undefined|| order.tbl_forma_pagamento_id == null || isNaN(order.tbl_forma_pagamento_id) ||
         order.data_pagamento == "" || order.data_pagamento == undefined || order.data_pagamento == null || order.data_pagamento.length > 19
     ){
         return message.ERROR_REQUIRED_FIELDS
     } else {
-       
-        const event = await stripe.handlePayment(order, sig);
-        if (!event) return 
   
         await createPayment(order, event.payment_intent);
         return { recieved: true };
@@ -87,6 +91,8 @@ const confirmPayment = async (order, sig) => {
     }
     
 };
+
+
 
 module.exports = {
     listAllPayments,
